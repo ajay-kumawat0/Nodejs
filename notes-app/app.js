03371896e0b393b975c399ee3c5b02635d74bbb3
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-09 09:23:38 +0000

    Commit

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,9 @@
-// const chalk = require('chalk')
-const yargs = require('yargs');
-const notes = require('./notes.js');
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
 
 // Customize yargs version
-yargs.version('1.1.0');
+yargs.version('1.1.0')
 
 // Create add command
 yargs.command({
@@ -21,7 +21,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.addNote(argv.title, argv.body)
     }
 })
@@ -37,7 +37,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.removeNote(argv.title)
     }
 })
@@ -46,8 +46,8 @@ yargs.command({
 yargs.command({
     command: 'list',
     describe: 'List your notes',
-    handler: function () {
-        console.log('Listing out all notes')
+    handler() {
+        notes.listNotes()
     }
 })
 
@@ -55,9 +55,16 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler: function () {
-        console.log('Reading a note')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
     }
 })
 
-yargs.parse();
+yargs.parse()
