c583ccf11811713cc14ee2050dda61557a240c6f
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-10 05:31:00 +0000

    weather api

--- 03_weather-app/01_async.js
+++ 03_weather-app/01_async.js
@@ -1,7 +1,29 @@
-console.log("Start")
+// // console.log("Start")
 
-setTimeout(()=>{
-    console.log("Run After 2 second");
-}, 2000)
+// setTimeout(()=>{
+//     // console.log("Run After 2 second");
+// }, 2000)
 
-console.log("End")
+// // console.log("End");
+
+
+
+// const listLocation = (location)=>{
+//     location.forEach((location) => {
+//         console.log(location);
+//     });
+// }
+
+// const location = ['Gujarat', 'Rajasthan']
+
+// listLocation(location);
+
+
+const request = require('request');
+
+const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596'
+
+request({url}, (err, res)=>{
+    const data = JSON.parse(res.body)
+    console.log(data.current.temperature);
+})
