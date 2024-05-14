1daf5086b6a6d8522196314ccc80d0304c62432f
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-14 06:30:22 +0000

    task-app

--- 05_task-app/config/mongoose.js
+++ 05_task-app/config/mongoose.js
@@ -4,3 +4,5 @@ mongoose.connect('mongodb://127.0.0.1/CRUD-1');
 const db = mongoose.connection;
 
 db.once('open', (err)=> err ? console.log(err) : console.log('DB is connected'));
+
+module.exports = db;
