49a719437651d2ac2840a1acae5e4c2cf4767b5d
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-15 05:58:20 +0000

    login

--- 05_task-app/routes/user.js
+++ 05_task-app/routes/user.js
@@ -5,6 +5,8 @@ const userController = require('../controller/user');
 
 routs.post('/user', userController.user);
 
+routs.post('/login', userController.login);
+
 routs.get('/getUser', userController.getUser);
 
 routs.put('/updateUser/:id', userController.updateUser);
