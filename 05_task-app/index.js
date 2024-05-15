1920f8d8dcd1c7ec69527cdd696a74624eb14b68
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-15 06:47:59 +0000

    passport-jwt

--- 05_task-app/index.js
+++ 05_task-app/index.js
@@ -2,9 +2,25 @@ const express = require('express');
 const port = 8001;
 const app = express();
 
-app.use(express.urlencoded())
+app.use(express.urlencoded());
+
+const passport = require('passport')
+const passport_jwt = require('./config/passportJwt');
+const session = require('express-session');
 
 const db = require('./config/mongoose');
+
+app.use(session({
+    name : 'PROJECT-DEMO',
+    secret : 'USER',
+    saveUninitialized : true,
+    cookie : {
+        maxAge : 1000*60*60
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', require('./routes/user'));
 
