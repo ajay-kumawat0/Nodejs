b45f982e8ea5075c7d1c3810f7f09cc6618a5b60
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-16 06:19:17 +0000

    File upload

--- 05_task-app/controller/user.js
+++ 05_task-app/controller/user.js
@@ -1,41 +1,42 @@
 const user = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const sharp = require('sharp');
 
 module.exports.user = async (req, res) => {
     try {
         let data = await user.findOne({ email: req.body.email })
-        if(data){
-            return res.status(200).json({msg : 'User already registerd', status : 0});
-        } else{
+        if (data) {
+            return res.status(200).json({ msg: 'User already registerd', status: 0 });
+        } else {
             req.body.password = await bcrypt.hash(req.body.password, 10)
             let userData = await user.create(req.body);
-            if(userData){
-                return res.status(200).json({msg : `User register successfully`, status : 1, UserData : userData});
+            if (userData) {
+                return res.status(200).json({ msg: `User register successfully`, status: 1, UserData: userData });
             }
-            else{
-                return res.status(200).json({msg : `You can't registered..! Something wrong`, status : 0});
+            else {
+                return res.status(400).json({ msg: `You can't registered..! Something wrong`, status: 0 });
             }
         }
     } catch (err) {
         console.log(err);
-        return res.status(200).json({msg : 'Something wrong', status : 0});
+        return res.status(400).json({ msg: 'Something wrong', status: 0 });
     }
 }
 
-module.exports.login = async(req,res)=>{
+module.exports.login = async (req, res) => {
     try {
-        let userData = await user.findOne({email : req.body.email});
-        if(userData){
-            if(await bcrypt.compare(req.body.password, userData.password)){
-                let token = await jwt.sign({userData }, 'USER', {expiresIn : '1h'});
-                return res.status(200).json({ msg: 'User login successfully', status: 1, Login : userData , Token : token});
+        let userData = await user.findOne({ email: req.body.email });
+        if (userData) {
+            if (await bcrypt.compare(req.body.password, userData.password)) {
+                let token = await jwt.sign({ userData: userData }, 'USER', { expiresIn: '1h' });
+                return res.status(200).json({ msg: 'User login successfully', status: 1, Login: userData, Token: token });
             }
-            else{
+            else {
                 return res.status(400).json({ msg: 'Incorrect password', status: 0 });
             }
         }
-        else{
+        else {
             return res.status(400).json({ msg: 'Invalid email', status: 0 });
         }
     } catch (err) {
@@ -44,62 +45,77 @@ module.exports.login = async(req,res)=>{
     }
 }
 
-module.exports.getUser = async(req,res)=>{
+module.exports.getUser = async (req, res) => {
     try {
-        let userData = await user.find({});
-        if(userData){
-            return res.status(200).json({msg : 'Record are here', status : 1, UserData : userData});
+        let userData = await user.find(req.user);
+        // console.log(userData);
+        if (userData) {
+            // console.log(req.user.token);
+            return res.status(200).json({ msg: 'Record are here', status: 1, UserData: userData });
         }
-        else{
-            return res.status(200).json({msg : 'Record not found', status : 0});
+        else {
+            return res.status(400).json({ msg: 'Record not found', status: 0 });
         }
     } catch (err) {
         console.log(err);
-        return res.status(200).json({msg : 'Something wrong', status : 0});
+        return res.status(400).json({ msg: 'Something wrong', status: 0 });
+    }
+}
+
+module.exports.getAllUser = async (req, res) => {
+    try {
+        let userData = await user.find({});
+        if (userData) {
+            return res.status(200).json({ msg: 'Record are here', status: 1, UserData: userData });
+        }
+        else {
+            return res.status(400).json({ msg: 'Record not found', status: 0 });
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({ msg: 'Something wrong', status: 0 });
     }
 }
 
 module.exports.updateUser = async (req, res) => {
     try {
-        let data = await user.findById(req.params.id)
-        if (data) {
-            let updateData = await user.findByIdAndUpdate(req.params.id, req.body);
-            console.log(updateData);
-            if (updateData) {
-                let updated = await user.findById(req.params.id);
-                console.log(updated);
-                return res.status(200).json({ msg: 'Record updated successfully', status: 0 , updated : updated});
-            }
-            else {
-                return res.status(200).json({ msg: 'Record not update', status: 0 });
-            }
+        let updateData = await user.findByIdAndUpdate(req.user, req.body);
+        if (updateData) {
+            let updated = await user.findById(req.user);
+            return res.status(400).json({ msg: 'Record updated successfully', status: 0, updated: updated });
         }
         else {
-            return res.status(200).json({ msg: 'Record not found', status: 0 });
+            return res.status(400).json({ msg: 'Record not update', status: 0 });
         }
+
     } catch (err) {
         console.log(err);
-        return res.status(200).json({ msg: 'Something wrong', status: 0 });
+        return res.status(400).json({ msg: 'Something wrong', status: 0 });
     }
 }
 
-module.exports.deleteUser = async(req,res)=>{
+module.exports.deleteUser = async (req, res) => {
     try {
-        let data = await user.findById(req.params.id)
-        if(data){
-            let deleteData = await user.findByIdAndDelete(req.params.id)
-            if(deleteData){
-                return res.status(200).json({ msg: 'Record delete successfully', status: 1, Delete : data });
-            }
-            else{
-                return res.status(200).json({ msg: 'Record not delete', status: 0 });
-            }
+        let deleteData = await user.findByIdAndDelete(req.user);
+        if (deleteData) {
+            return res.status(200).json({ msg: 'Record delete successfully', status: 1, Delete: deleteData });
         }
-        else{
-            return res.status(200).json({ msg: 'Record not found', status: 0 });
+        else {
+            return res.status(400).json({ msg: 'Record not delete', status: 0 });
         }
     } catch (err) {
         console.log(err);
-        return res.status(200).json({ msg: 'Something wrong', status: 0 });
+        return res.status(400).json({ msg: 'Something wrong', status: 0 });
     }
+}
+
+module.exports.userProfile = async (req, res) => {
+    let profileBuffer = await sharp(req.file.buffer).resize({width : 200 , height : 200}).png().toBuffer();
+    console.log(profileBuffer);
+    // req.user.avatar = req.body.avatar
+    // console.log(req.user);
+    // await req.user.save()
+    res.status(200).json({ msg: 'file uploaded', status: 1 })
+    }, (error, req, res, next) => {
+    res.status(200).json({ error: error.message })
 }
--- 05_task-app/routes/user.js
+++ 05_task-app/routes/user.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const routs = express.Router();
 
+// multer
+
+const multer = require('multer');
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(png|jpg)$/)) {
+            return cb(new Error('Upload a png and jpg file'))
+        }
+        cb(undefined, true);
+    }
+})
+
+// app.post('/userProfile', upload.single('avatar'), async (req, res) => {
+//     res.status(200).json({ msg: 'file uploaded', status: 1 })
+// }, (error, req, res, next) => {
+//     res.status(200).json({ error: error.message})
+// })
+
 const userController = require('../controller/user');
 const passport = require('passport');
 
@@ -10,12 +31,16 @@ routs.post('/login', userController.login);
 
 routs.get('/getUser',passport.authenticate('jwt', {failureRedirect :'/failLogin'}),userController.getUser);
 
+routs.get('/getAllUser',userController.getUser);
+
 routs.get('/failLogin', async(req,res)=>{
     return res.status(400).json({ msg: 'First you have to login..!', status: 0 })
 })
 
-routs.put('/updateUser/:id', userController.updateUser);
+routs.patch('/updateUser', passport.authenticate('jwt', {failureRedirect :'/failLogin'}) ,userController.updateUser);
 
-routs.delete('/deleteUser/:id', userController.deleteUser);
+routs.delete('/deleteUser',passport.authenticate('jwt', {failureRedirect :'/failLogin'}) ,userController.deleteUser);
+
+routs.post('/userProfile', passport.authenticate('jwt', {failureRedirect :'/failLogin'}),upload.single('avatar'), userController.userProfile);
 
 module.exports = routs;
