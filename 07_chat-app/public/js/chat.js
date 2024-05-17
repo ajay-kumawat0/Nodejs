4f48eb8658eeebc3600b154c33fe4369f9cfc540
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-17 12:03:58 +0000

    chat app

--- 07_chat-app/public/js/chat.js
+++ 07_chat-app/public/js/chat.js
@@ -2,3 +2,7 @@ const socket = io();
 socket.on('countUpdated', (count)=>{
     console.log('Count Updated....', count);
 })
+
+socket.on('countUpdated', (count)=>{
+    console.log('Updated Count');
+})
