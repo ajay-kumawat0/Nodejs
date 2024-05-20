6f70e9d8a1d6e03dd27dccb977b8de0169305cbd
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-20 04:46:36 +0000

    broadcast

--- 07_chat-app/public/js/chat.js
+++ 07_chat-app/public/js/chat.js
@@ -1,6 +1,31 @@
 const socket = io();
-socket.on('countUpdated', (count)=>{
-    console.log('Count Updated....', count);
 
-    socket.emit('increment');
+socket.on('message', (message) => {
+    console.log(message);
 })
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    // const message = document.querySelector('input').value;
+    const message = e.target.elements.msg.value;
+    socket.emit("sendMessage", message);
+})
+
+
+
+
+
+
+
+// ****************************************************************************************
+// server to client
+// socket.on('countUpdated', (count)=>{
+//     console.log('Count Updated....', count);
+// })
+
+// // client-side  // client to server
+// document.querySelector('#increase').addEventListener('click', ()=>{
+//     console.log("Button is clicked");
+//     socket.emit('increment');
+// })
--- 07_chat-app/src/index.js
+++ 07_chat-app/src/index.js
@@ -10,11 +10,25 @@ const io = socketio(server);
 const port = process.env.PORT || 8001;
 app.use(express.static(path.join(__dirname, '../public')));
 
-let count = 0;
+// let count = 0;
 
 // server-side
-io.on('connection', (socket) => {
-    console.log('Web socket is call from server');
+io.on('connection', (socket) => {  // io.on => it is used to only when an cliet is connected not used for when client is disconnected
+    // console.log('Web socket is call from server');
+
+    socket.emit('message', 'Welcome to chat app');  // 'Hello' => name of the event  //  'Welcome to chat app' => callback msg provide to client 
+    socket.broadcast.emit('message', 'A new user is connected..!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message); // send msg to every client who is connected to server
+    })
+
+    // it is run when user is left or diconnected
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left');
+    }) // 'disconnect' => this is build in functionalty
+
+    /* 
 
     // server to client 
     socket.emit('countUpdated', count);
@@ -23,8 +37,10 @@ io.on('connection', (socket) => {
     socket.on('increment', () => {
         count++;
         // socket.emit('countUpdated', count); // it is used to check separate - separate
-        io.emit('countUpdated', count); // this one is used to everyone single connection
+        io.emit('countUpdated', count); // this one is used to everyone single connection  // io.emit => text capture from another client or check how much client added to server
     })
+
+    */
 })
 
 server.listen(port, (err)=>{
